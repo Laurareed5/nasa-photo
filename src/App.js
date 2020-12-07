b84e4bb88b86d70d3065c7d368d24cb5482008a7
Laura reed <pilesoflaura@gmail.com>
2020-12-06 19:01:12 -0700

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import NasaData from "./Components/NasaData";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NasaData />
+    </div>
+  );
+}
+
+export default App;
